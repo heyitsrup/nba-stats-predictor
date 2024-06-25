1fe6bc52fda01d1b1123a82924f76a3388a45211
Rup Singh <rs02002@surrey.ac.uk>
2024-06-25 16:54:47 +0100

    Update loading page

--- performance_predictor_frontend/src/components/Loading.tsx
+++ performance_predictor_frontend/src/components/Loading.tsx
@@ -1,19 +1,45 @@
 'use client'
 
+import React, { useState, useEffect } from 'react';
+
 const Loading = () => {
-    return (
-      <div className="fixed top-0 left-0 z-50 w-full h-full overflow-hidden bg-nba-white bg-opacity-75 flex items-center justify-center">
-        <div className="flex items-center">
-          <svg className="animate-spin h-10 w-10 text-white mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
-          <span className="text-white text-xl">Loading...</span>
-        </div>
+  const [loadingMessage, setLoadingMessage] = useState("Loading...");
+
+  useEffect(() => {
+    const messages = [
+      "Collecting historical data...",
+      "Training neural network...",
+      "Predicting future metrics...",
+      "Loading"
+    ];
+
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      setLoadingMessage(messages[currentIndex]);
+      currentIndex++;
+
+      if (currentIndex === messages.length) {
+        currentIndex = 0;
+      }
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full h-full overflow-hidden bg-nba-white bg-opacity-75 flex items-center justify-center">
+      <div className="flex items-center">
+        <svg className="animate-spin h-10 w-10 text-white mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <span className="text-white text-xl">{loadingMessage}</span>
       </div>
-    );
-  };
-  
-  export default Loading;
+    </div>
+  );
+};
+
+export default Loading;
   
   
